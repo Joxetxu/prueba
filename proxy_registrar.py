1b97e5543628e8fb5389850d5b113c9becbc03db
irenerl10 <irene17a@gmail.com>
2018-06-24 20:56:13 +0200

    Falta log y acabarlo

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -28,10 +28,58 @@ class SmallSMILHandler(ContentHandler):
         """
         if name in self.Element:
             for elemento in self.Element[name]:
-                self.atr[elemento] = attrs.get(elemento, '')
-                self.list.append(elemento.atr)
+                self.list[name + elemento] = attrs.get(elemento, "")
         def get_tags(self):
             return self.list
+
+class ProxyRegisterHandler(socketserver.DatagramRequestHandler):
+    users = {}
+    passwds = {}
+
+    def register2json(self):
+        """me guarda el fichero en formato json"""
+        with open(database, 'w') as file:
+            json.dump(self.users, file)
+
+    def json2registered(self):
+        try:
+            with open("register.json", "r") as file:
+                json.load(self.users, file)
+
+        except (NameError, FileNotFoundError, AttributeError):
+            pass
+
+    def chec_passwd(self, user, nonce):
+        contra = self.passwd[user]
+        h = hashlib.sha1(bytes(contra, "utf-8"))
+        h.update(bytes(nonce, "utf-8"))
+        return h.hexdigest()
+
+    def time_expired(self):
+        dicc = {}
+        if self.users:
+            for user in dicc:
+                if dicc[user] < time.time():
+                    del self.users[user]
+            for user in self.users:
+                dicc[user] = self.dicc[user][3]
+            self.update_database()
+
+    def read_passwd(self):
+
+        with open(passwd_database, "r") as file:
+            for line in data_file:
+                key = line.split(" ")[0]
+                value = line.split(" ")[1]
+                self.passwdss[key] = value
+
+    def register(self, DATA):
+
+        nonce = '89898347853"
+        self.read_passwd()
+
+
+
 
 if __name__ =='__main__':
     parser = make_parser()
